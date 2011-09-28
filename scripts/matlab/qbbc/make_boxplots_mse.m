af786d2c5a3c7dc9c3edd6702975e0c6555bf642
Faris Alqadah <faris.alqadah@gmail.com>
2011-09-28 12:18:28 -0400

    added matlab script for qbbc mse scatter plots

--- scripts/matlab/qbbc/make_boxplots_mse.m
+++ scripts/matlab/qbbc/make_boxplots_mse.m
@@ -13,17 +13,22 @@
 %  Output: box plot comparing all the methods
 function make_boxplots_mse(file_dirs,n,method_names,full_matrix)
     mse = [];
-    for i=1:numel(file_dir)
+    for i=1:numel(file_dirs)
         curr_mse = [];
         for j=1:n
-            cluster_file_name = [file_dir{i} j '.concepts'];
+            cluster_file_name = [file_dirs{i} num2str(j) '.concepts'];
             cluster_name_file_name=[cluster_file_name '.names'];
             [cluster cluster_name] = read_nclusters(cluster_file_name,cluster_name_file_name);
-            matrix = full_matrix(cluster{1},cluster{2}];
-            curr_mse = [curr_mse mean_square_error_sparse(matrix)];
+            if numel(cluster) > 0
+                if numel(cluster{1}{2}) > 0 && numel(cluster{1}{1}) > 0
+                    matrix = full_matrix(cluster{1}{2},cluster{1}{1});
+                    curr_mse = [curr_mse mean_square_error_sparse(matrix)];
+                end
+            end
         end
         mse = [mse; curr_mse];
     end
+    mse = mse';
     boxplot(mse,'labels',method_names);
     ylabel('MSE');
 
